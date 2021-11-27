31acae204e5e9e96e2b96d58dc76061009083915
Nelson Chaves <nelsonchavespro@gmail.com>
2021-11-26 23:31:39 -0800

    image validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,10 @@
 class Event < ApplicationRecord
+  validates :name, :location, presence: true
+  validates :description, length: { minimun: 25 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :capacity, numericality: { only_integer: true, greater_than: 0 }
+  validates :image_file_name, format: { with: /\w+\.(jpg|png)\z/i, message: "must be a JPG or PNG image" }
+  
   def free?
     price.blank? || price.zero?
   end
