ef9d8deced4f417dcaa1f5c13ee29b386627415d
Nelson Chaves <nelsonchavespro@gmail.com>
2021-11-30 22:33:29 -0800

    add categories

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
   before_action :require_signin, except: [:index, :show]
   before_action :require_admin, except: [:index, :show]
-  
+
   def index
     @events = Event.upcoming
   end
@@ -9,6 +9,7 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
     @likers = @event.likers
+    @categories = @event.categories
     if current_user
       @like = current_user.likes.find_by(event_id: @event.id)
     end
